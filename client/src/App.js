2c4c47e1af8aae9f54e08f9b1b43bc7aa45bd34a
ksyus-h <ksyusuf85@gmail.com>
2024-08-24 21:45:21 +0300

    redux entegre edildi. redux entegre, fakat daha iyi hale getirilecek.

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,57 @@ import React, { useState, useEffect } from 'react';
 import AllNotesPage from './components/AllNotesPage';
 import NewNotePage from './components/NewNotePage';
 import EditNotePage from './components/EditNotePage';
-import { fetchNotes, addNote, updateNote, deleteNote } from './api/notesFromAxios'; 
+// import { fetchNotes, addNote, updateNote, deleteNote } from './api/notesFromAxios';
+import { useDispatch, useSelector  } from 'react-redux';
+import { fetchNotesAndCategories, addNote, editNote, deleteNote } from './redux/DataActions';
 
 const App = () => {
+
   const [page, setPage] = useState('all');
   const [notesData, setNotesData] = useState([]);
-  const [categoriesData, setCategoriesData] = useState([]);
+  // const [categoriesData, setCategoriesData] = useState([]);
   const [loading, setLoading] = useState(true);
   const [currentNote, setCurrentNote] = useState(null);
 
-  useEffect(() => {
-    const loadNotes = async () => {
-      try {
-        const Alldata = await fetchNotes();
-        setNotesData(Alldata.notes);
-        setCategoriesData(Alldata.categories);
-      } catch (error) {
-        console.error('Failed to load notes:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
+  // useEffect(() => {
+  //   const loadNotes = async () => {
+  //     try {
+  //       const Alldata = await fetchNotes();
+  //       setNotesData(Alldata.notes);
+  //       setCategoriesData(Alldata.categories);
+  //     } catch (error) {
+  //       console.error('Failed to load notes:', error);
+  //     } finally {
+  //       setLoading(false);
+  //     }
+  //   };
 
-    loadNotes();
-  }, []);
+  //   loadNotes();
+  // }, []);
+
+  const dispatch = useDispatch();
+  const { notes, categories } = useSelector(state => ({
+    notes: state.notes.notes,
+    categories: state.categories.categories,
+  }));
+
+  // loadData fonksiyonunu tanımladık.
+  // bu şekilde diğer kısımlarda da bunu kullanabileceğiz.
+  // fakat vt.den verilerin çekilmesini beklerken sayfa yükleniyor ve
+  // sayfa yüklendiğinde milisaniyeler ile verinin ana sayfaya düşüşünü
+  const loadData = async () => {
+    try {
+      await dispatch(fetchNotesAndCategories());
+    } catch (error) {
+      console.error('Failed to load notes and categories:', error);
+    } finally {
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, [dispatch]);
 
   const handleAddNoteClick = () => {
     setPage('new');
@@ -39,21 +66,24 @@ const App = () => {
 
   const handleSaveNote = async (newNote) => {
     try {
-      const savedNote = await addNote(newNote);
-      setNotesData([...notesData, savedNote]);
+      // const savedNote = await addNote(newNote);
+      // setNotesData([...notesData, savedNote]);
+      // loadData();
+      dispatch(addNote(newNote));
       setPage('all');
     } catch (error) {
       console.error('Failed to save note:', error);
     }
   };
 
-  const handleUpdateNote = async (updatedNote) => {
+  const handleUpdateNote = async (gelen_not) => {
     try {
-      const updated = await updateNote(updatedNote);
-      setNotesData(notesData.map(note => note.id === updated.id ? updated : note));
+      // const updated = await updateNote(updatedNote);
+      // setNotesData(notesData.map(note => note.id === updated.id ? updated : note));
       // düzenlenmiş veriyi ana sayfaya gönderir. bunu yapmazsan sayfayı yenilemeden
       // güncellediğin notun güncel halini göremezsin.
       // hem bu şekilde yeniden tüm notları çekmeye gerek kalmaz.
+      dispatch(editNote(gelen_not));
       setPage('all');
     } catch (error) {
       console.error('Failed to update note:', error);
@@ -62,18 +92,22 @@ const App = () => {
 
   const handleDeleteNote = async (deleted_node) => {
     try {
-      await deleteNote(deleted_node);
-      setNotesData(notesData.filter(note => note._id !== deleted_node._id));
+      // await deleteNote(deleted_node);
+      // setNotesData(notesData.filter(note => note._id !== deleted_node._id));
       // bu sefer tüm verileri yeniden çekip yüklemek yerine ilgili notu filtre ile
       // mevcut oturumda kaldırmayı tercih ettim.
       // sayfa yenilendiğinde zaten veritabanında olmayacağı için bu veri, gözükmeyecek.
+      console.log(deleted_node)
+      dispatch(deleteNote(deleted_node));
+      setPage('all');
     } catch (error) {
       console.error('Failed to delete note:', error);
     }
   };
 
   // tüm kategorileri id değerleri ile çekmiştik, şimdi alfabetik sıralayıp öyle paylaşıyoruz
-  const Allcategories = categoriesData.sort((a, b) => a.name.localeCompare(b.name));
+  // redux değişmezlik kuralından dolayı değişkeni değiştirmeden kopyası ile işlem yapıyoruz.
+  const Allcategories = [...categories].sort((a, b) => a.name.localeCompare(b.name));
 
   return (
     <div className="App">
@@ -81,7 +115,7 @@ const App = () => {
         <div>Loading...</div>
       ) : page === 'all' ? (
         <AllNotesPage 
-          notes={notesData}
+          notes={notes}
           categories={Allcategories}
           onAddNote={handleAddNoteClick} 
           onEditNote={handleEditNoteClick} 
