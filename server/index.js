cd02b335373260a0988d4462c80cc5362ab15c0a
ksyus-h <ksyusuf85@gmail.com>
2025-04-01 19:42:33 +0300

    cors düzeltmesi

--- server/index.js
+++ server/index.js
@@ -51,7 +51,7 @@ const corsOptions = {
       callback(new Error('Not allowed by CORS'));
     }
   },
-  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type']
 };
 
